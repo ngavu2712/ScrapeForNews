4990a01798530bea296d8ddffbe96e02dd1c2b2f
Anna Vu <ngavu2712@gmail.com>
2020-06-06 16:03:06 -0700

    changed the connection string for mongoDB, added Heroku config to the connection

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Connect to MongoDB
-mongoose.connect("mongodb://localhost/news", {useNewUrlParser: true});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news"
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
 //===============================================================================================================
 //Start the server
