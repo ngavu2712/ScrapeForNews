37f4e0f2db1c5fda13983252c5968e559969476d
Anna Vu <ngavu2712@gmail.com>
2020-06-12 14:20:45 -0700

    using ajax in app.js to make a get request in order ot retrieve scraped data from route /scrape

---
+++ public/app.js
@@ -0,0 +1,13 @@
+$("#scrape").on("click", function(){
+    event.preventDefault()
+    alert("Added 5 New Articles")
+
+    $.ajax({
+        url: "/scrape",
+        method: "GET",
+        //data: Article
+    }).then(function(data){
+        console.log(data)
+    })
+
+})
